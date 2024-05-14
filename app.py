525d70c1a613d26629bcfb3d1969bd42f7fbc8b7
keithtyser <38081-keith_tyser@users.noreply.gitlab.aicrowd.com>
2024-05-14 00:01:20 -0400

    updated ui, added leaderboard

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import gradio as gr
 from utils import process_paper
 import os
 import logging
-import html 
+import html
 from logging_config import setup_logging
 
-
-setup_logging()  # Ensure logging is initialized
-# Define global variables for directories and API keys
+setup_logging()
 paper_dir = 'path_to_temp_storage'
 prompt_dir = 'iclr2024'
 api_keys = {
@@ -17,26 +15,26 @@ api_keys = {
     'commandr_api_key': os.environ.get('cohere_api_key')
 }
 
-# Configure whether to use real API or not
-use_real_api = False  # Set this to True to use real APIs, False to use dummy data
+use_real_api = False
+
 
 def review_papers(pdf_file):
     logging.info(f"Received file type: {type(pdf_file)}")
     if use_real_api:
-        reviews = process_paper(pdf_file, paper_dir, prompt_dir, api_keys)
+        reviews, selected_models = process_paper(
+            pdf_file, paper_dir, prompt_dir, api_keys)
         processed_reviews = []
         for review in reviews:
             processed_review = {}
             for section in review:
-                if ':' in section:  # Ensure there is a colon to split on
-                    key, value = section.split(':', 1)  # Split on the first colon only
-                    # Replace newline characters with <br> for HTML line breaks
+                if ':' in section:
+                    key, value = section.split(':', 1)
                     processed_value = value.strip().replace('\n', '<br>')
-                    processed_review[key.strip()] = html.escape(processed_value)  # Ensure HTML escaping
+                    processed_review[key.strip()] = html.escape(
+                        processed_value)
             processed_reviews.append(processed_review)
         reviews = processed_reviews
     else:
-        # Dummy reviews for testing with structured sections
         reviews = [
             {
                 "Summary": "This is a placeholder review for Model 1. The paper explores advanced methodologies in reinforcement learning applied to autonomous driving systems, proposing significant enhancements to decision-making algorithms that could improve safety and operational efficiency. The authors provide a detailed analysis of the current limitations of existing systems and suggest innovative solutions that could transform the field.",
@@ -65,15 +63,7 @@ def review_papers(pdf_file):
                 "Code of Conduct": "There are no violations of the code of conduct noted. The research upholds ethical standards and maintains transparency in methodologies and data usage, contributing to its integrity and the trustworthiness of the findings."
             }
         ]
-    processed_reviews = []
-    for review in reviews:
-        processed_review = {}
-        for key, value in review.items():
-            # Replace newline characters with <br> for HTML line breaks and escape HTML
-            processed_value = value.strip().replace('\n', '<br>')
-            processed_review[key.strip()] = html.escape(processed_value)  # Ensure HTML escaping
-        processed_reviews.append(processed_review)
-    reviews = processed_reviews
+        selected_models = ['model1-placeholder', 'model2-placeholder']
 
     review_texts = []
     for review in reviews:
@@ -82,8 +72,13 @@ def review_papers(pdf_file):
             formatted_review += f"<div class='review-section'><strong>{section}:</strong> <span>{html.unescape(content)}</span></div>"
         formatted_review += "</div>"
         review_texts.append(formatted_review)
+
+    model_a = selected_models[0]
+    model_b = selected_models[1]
+
     logging.debug(f"Final formatted reviews: {review_texts}")
-    return review_texts
+    return review_texts[0], review_texts[1], gr.update(visible=True), gr.update(visible=True), model_a, model_b
+
 
 def setup_interface():
     logging.debug("Setting up Gradio interface.")
@@ -125,44 +120,135 @@ def setup_interface():
         width: 300px;
         margin: auto;
     }
+    .gr-button-small {
+        width: 150px;
+        height: 40px;
+        font-size: 16px;
+    }
     """
     with gr.Blocks(css=css) as demo:
-        gr.Markdown("## Reviewer Arena")
-        gr.Markdown("Upload an academic paper to get reviews from two randomly selected LLMs.")
-        with gr.Row():
-            file_input = gr.File(label="Upload Academic Paper")
-            submit_button = gr.Button("Submit!")
-        with gr.Row():
-            with gr.Column():
-                gr.HTML("<div class='model-label'>Model A</div>")
-                review1 = gr.Markdown()  
-            with gr.Column():
-                gr.HTML("<div class='model-label'>Model B</div>")
-                review2 = gr.Markdown() 
+        with gr.Tabs():
+            with gr.TabItem("Reviewer Arena"):
+                gr.Markdown("## Reviewer Arena")
+                gr.Markdown(
+                    "Upload an academic paper to get reviews from two randomly selected LLMs.")
+                with gr.Row():
+                    file_input = gr.File(label="Upload Academic Paper")
+                    submit_button = gr.Button(
+                        "Submit!", elem_id="submit-button")
+                with gr.Row():
+                    with gr.Column():
+                        gr.HTML("<div class='model-label'>Model A</div>")
+                        review1 = gr.Markdown()
+                    with gr.Column():
+                        gr.HTML("<div class='model-label'>Model B</div>")
+                        review2 = gr.Markdown()
 
-        # Voting options
-        vote_options = ["👍 A is better", "👍 B is better", "👔 Tie", "👎 Both are bad"]
-        vote = gr.Radio(label="Vote on the best model", choices=vote_options, value="Tie")
-        vote_button = gr.Button("Submit Vote")
+                vote_options = ["👍 A is better",
+                                "👍 B is better", "👔 Tie", "👎 Both are bad"]
+                vote = gr.Radio(label="Vote on the best model",
+                                choices=vote_options, value="Tie", visible=False)
+                vote_button = gr.Button("Submit Vote", visible=False)
+                vote_message = gr.HTML("", visible=False)
+                another_paper_button = gr.Button(
+                    "Review another paper", visible=False)
 
-        def handle_vote(vote):
-            print(f"Vote received: {vote}")
-            return f"Vote for '{vote}' received!"
+                model_identity_message = gr.HTML("", visible=False)
 
-        vote_button.click(fn=handle_vote, inputs=vote, outputs=gr.Textbox(visible=False))
+                def handle_vote(vote, model_a, model_b):
+                    print(f"Vote received: {vote}")
+                    message = f"<p>Thank you for your vote!</p><p>Model A: {model_a}</p><p>Model B: {model_b}</p>"
+                    return gr.update(value=message, visible=True), gr.update(visible=False), gr.update(visible=False), gr.update(visible=True)
 
-        submit_button.click(
-            fn=review_papers, 
-            inputs=[file_input], 
-            outputs=[review1, review2]
-        )
+                vote_button.click(fn=handle_vote, inputs=[vote, model_identity_message, model_identity_message], outputs=[
+                                  vote_message, vote, vote_button, another_paper_button])
+
+                submit_button.click(
+                    fn=review_papers,
+                    inputs=[file_input],
+                    outputs=[review1, review2, vote, vote_button,
+                             model_identity_message, model_identity_message]
+                )
+
+                another_paper_button.click(
+                    fn=lambda: None, inputs=None, outputs=None, js="() => { location.reload(); }")
+            with gr.TabItem("Leaderboard"):
+                gr.Markdown("## Leaderboard")
+                leaderboard_html = """
+                    <table style="width:100%; border: 1px solid #444; border-collapse: collapse; font-family: Arial, sans-serif; background-color: #2b2b2b;">
+                        <thead>
+                            <tr style="border: 1px solid #444; padding: 12px; background-color: #1a1a1a;">
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">Rank</th>
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">Model</th>
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">Arena Elo</th>
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">95% CI</th>
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">Votes</th>
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">Organization</th>
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">License</th>
+                                <th style="border: 1px solid #444; padding: 12px; color: #ddd;">Knowledge Cutoff</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr style="border: 1px solid #444; padding: 12px;">
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">1</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">GPT-4-Turbo-2024-04-09</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">1258</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">+3/-3</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">44592</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">OpenAI</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Proprietary</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2023/12</td>
+                            </tr>
+                            <tr style="border: 1px solid #444; padding: 12px;">
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">GPT-4-1106-preview</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">1252</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">+2/-3</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">76173</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">OpenAI</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Proprietary</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2023/4</td>
+                            </tr>
+                            <tr style="border: 1px solid #444; padding: 12px;">
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Gemini 1.5 Pro API-0409-Preview</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">1249</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">+3/-3</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">61011</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Google</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Proprietary</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2023/11</td>
+                            </tr>
+                            <tr style="border: 1px solid #444; padding: 12px;">
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Claude 3 Opus</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">1248</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">+2/-2</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">101063</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Anthropic</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Proprietary</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2023/8</td>
+                            </tr>
+                            <tr style="border: 1px solid #444; padding: 12px;">
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">3</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">GPT-4-0125-preview</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">1246</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">+3/-2</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">70239</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">OpenAI</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">Proprietary</td>
+                                <td style="border: 1px solid #444; padding: 12px; color: #ddd;">2023/12</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                """
+                gr.HTML(leaderboard_html)
+
     logging.debug("Gradio interface setup complete.")
     return demo
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     demo = setup_interface()
-    # BLOCK PATHS OF ALL THE FILES AND LAUNCH THE APP
-    
-    
-    demo.launch(auth=(os.environ.get('login_username'), os.environ.get('login_password')), share=True)
+    demo.launch()
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import fitz
 import os
 import logging
-import random 
+import random
 from models import Paper, PaperProcessor
+
 
 def extract_text_from_pdf(filename):
     with fitz.open(filename) as pdf_document:
@@ -11,39 +12,34 @@ def extract_text_from_pdf(filename):
             text += page.get_text()
     return text.encode('latin-1', 'replace').decode('latin-1')
 
+
 def process_paper(pdf_file, paper_dir, prompt_dir, api_keys):
-    logging.info(f"Processing file type in process_paper: {type(pdf_file)}")  # Log the type of the file here as well
+    logging.info(f"Processing file type in process_paper: {type(pdf_file)}")
     logging.debug(f"Starting to process paper: {pdf_file}")
-    # Ensure the directory exists
     os.makedirs(paper_dir, exist_ok=True)
 
-    # Handle file based on its type
     if isinstance(pdf_file, str):
-        # Assume pdf_file is a path to the PDF file
         pdf_path = pdf_file
     elif hasattr(pdf_file, 'name') and hasattr(pdf_file, 'read'):
-        # It's a file-like object
         pdf_path = os.path.join(paper_dir, pdf_file.name)
         with open(pdf_path, "wb") as f:
             f.write(pdf_file.read())
     else:
-        logging.error("Received object is neither a path nor a file-like object.")
-        return []
+        logging.error(
+            "Received object is neither a path nor a file-like object.")
+        return [], []
 
-    # Extract text from the PDF
     extracted_text = extract_text_from_pdf(pdf_path)
-    paper = Paper(pdf_file.name if hasattr(pdf_file, 'name') else os.path.basename(pdf_path), extracted_text)
+    paper = Paper(pdf_file.name if hasattr(pdf_file, 'name')
+                  else os.path.basename(pdf_path), extracted_text)
 
-    # Randomly select two models
     models = ['gpt', 'claude', 'gemini', 'commandr']
     selected_models = random.sample(models, 2)
 
-    # Process the paper with each selected model
     reviews = []
     for model in selected_models:
         processor = PaperProcessor(prompt_dir, model, **api_keys)
         review_text = processor.process_paper(paper)
-        #review_dict = {section.split(':')[0]: section.split(':')[1].strip() for section in review_text}
         reviews.append(review_text)
     logging.debug(f"Reviews generated: {reviews}")
-    return reviews
+    return reviews, selected_models
